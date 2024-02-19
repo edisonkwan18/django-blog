9cb17011199fddbffaac53c37346b6fb456a2e06
edisonkwan18 <edisonkwan18@gmail.com>
2024-02-19 09:55:37 +0000

    Change the new URLs import method and install django-summernote app Ver 0.8.2 for admin panel management

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '.herokuapp.com',
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
     'blog',
 
     #third-party
-    'bootstrap_admin',
+    'django_summernote',
 ]
 
 MIDDLEWARE = [
